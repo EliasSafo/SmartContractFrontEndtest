5fb78e5f15e698d1970bc202b4e78ad49344be2f
Elias Safo <eliassafo@gmail.com>
2024-05-26 11:34:54 +0200

    added metamask support

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://0.0.0.0:8000' }), // Set base URL for API requests
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000' }), // Set base URL for API requests
     endpoints: (builder) => ({
         payMoney: builder.mutation({
             query: (amount) => ({
